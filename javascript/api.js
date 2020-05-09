fffee37e1d538f2cef61c6f5833224bfca344cf9
Kaiden Munsey <kaidenmnsey@aol.com>
2020-05-08 21:15:27 -0400

    <adding array of topics and functions to load buttons to screen>

--- javascript/api.js
+++ javascript/api.js
@@ -1,14 +1,17 @@
 $(document).reday(function () {
-    let topics = []
+    let topics = ['Johny Cash', 'ACDC', 'The Doors', 'Led Zepplin', 'Beastie Boys', 'Frank Sinatra', 'Davis Bowie', 'Hank Williams', 'Queen', 'Daft Punk', 'Bruse Springsteen', 'Wham!']
 
 
     function displayGifsButton (){
         $("#gifButtonsView").empty();
         for (var i = 0; i < topics.length; i++) {
             let gifButton = $('<button>');
-            gifButton.addClass('')
-
-    }
+            gifButton.addClass('bands');
+            gifButton.addClass('btn bt-primary');
+            gifButton.attr('data-name', topics[i]);
+            gifButton.text(topics[1]);
+            $('#gifButtonsView').append(gifButton);
+        }
 
     }
 
