f40f2df8abc96f5a85aaeabb93a2bcdb65a1a484
Kaiden Munsey <kaidenmnsey@aol.com>
2020-05-08 21:06:24 -0400

    <adding array for topics and function to load buttons on screen>

---
+++ javascript/api.js
@@ -0,0 +1,45 @@
+$(document).reday(function () {
+    let topics = []
+
+
+    function displayGifsButton (){
+        $("#gifButtonsView").empty();
+        for (var i = 0; i < topics.length; i++) {
+            let gifButton = $('<button>');
+            gifButton.addClass('')
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
