432fcd1da5544e64ae158fb62764a7ed1795a642
Kaiden Munsey <kaidenmnsey@aol.com>
2020-05-09 09:34:12 -0400

    <adding functions to load gifs from buttons>

--- javascript/api.js
+++ javascript/api.js
@@ -1,18 +1,43 @@
-$(document).reday(function () {
-    let topics = ['Johny Cash', 'ACDC', 'The Doors', 'Led Zepplin', 'Beastie Boys', 'Frank Sinatra', 'Davis Bowie', 'Hank Williams', 'Queen', 'Daft Punk', 'Bruse Springsteen', 'Wham!']
+$(document).ready(function () { 
+    
+    let topics = ["Johny Cash", "ACDC", 'The Doors', 'Led Zepplin', 'Beastie Boys', 'Frank Sinatra', 'Davis Bowie', 'Hank Williams', 'Queen', 'Daft Punk', 'Bruse Springsteen', 'Wham!']
+    //array of music i have chosen that will be buttons to load gifs
 
-
-    function displayGifsButton (){
+    function displayGifsButton (){ //adds buttons from array to html page 
         $("#gifButtonsView").empty();
-        for (var i = 0; i < topics.length; i++) {
+        for (let i = 0; i < topics.length; i++) {
             let gifButton = $('<button>');
-            gifButton.addClass('bands');
+            gifButton.addClass('music');
             gifButton.addClass('btn bt-primary');
             gifButton.attr('data-name', topics[i]);
-            gifButton.text(topics[1]);
+            gifButton.text(topics[i]);
             $('#gifButtonsView').append(gifButton);
+      
         }
+    }
 
+    function dislayGifs () {
+        let music = $(this).attr('data-name');
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + music + "&api_key=dc6zaTOxFJmzC&limit=10";
+        console.log(queryURL)
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function (response){
+            $('#gifsview').empty();
+            let results = response.data;
+            if (results === "") {
+                alert('I guess they dont like that kind of music?')
+            }
+            for (let i=o; i<results.length; i++){
+
+            }   
+        })
+    
+    
+    
+    
     }
 
 
@@ -31,11 +56,13 @@ $(document).reday(function () {
 
 
 
+});
 
 
 
 
-})
+
+
 
 
 
